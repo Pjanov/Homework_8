54e3e640c16498fcf30c34b42cf63bae4ec6e462
Андрей <pjanov84@mail.ru>
2022-09-14 17:15:47 +0300

    Added Task_62

--- Task_62/Program.cs
+++ Task_62/Program.cs
@@ -5,3 +5,46 @@
 //  12 13 14 05
 //  11 16 15 06
 //  10 09 08 07
+
+Console.Clear();
+
+void FillArraySpiral(int[,] array, int n) // заполнит спирально массив n x n
+{
+    int y = 0, x = 0;
+    int value = 1;
+    for (int i = 0; i < n * n; i++)
+    {
+        int k = 0;
+        do array[y, x++] = value++; 
+        while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[y++, x] = value++;
+        for (k = 0; k < n - 1; k++) array[y, x--] = value++;
+        for (k = 0; k < n - 1; k++) array[y--, x] = value++;
+        ++y; ++x;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void PrintArray2(int[,] matr) //выводит в консоль двумерный массив
+{
+    for (int y = 0; y < matr.GetLength(0); y++)
+    {
+        for (int x = 0; x < matr.GetLength(1); x++)
+        {
+            if (matr[y, x] < 10)
+            {
+                Console.Write($"0{matr[y, x]} ");
+            }
+            else
+            {
+                Console.Write($"{matr[y, x]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = 4;
+int[,] array = new int[n, n];
+FillArraySpiral(array, n);
+PrintArray2(array);
