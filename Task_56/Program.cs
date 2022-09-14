c310f499a17ebb4e2115d2e9c284b96355e5e099
Андрей <pjanov84@mail.ru>
2022-09-14 17:14:19 +0300

    Added Task_56

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -8,9 +8,57 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер 
 // строки с наименьшей суммой элементов: 1 строка
 
+Console.Clear();
 
+int[,] FillArray2(int line, int col, int min, int max) //заполняет двумерный массив случайными числами от 1 до 9
+{
+    int[,] matr = new int[line, col];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matr;
+}
 
+void PrintArray2(int[,] matr) //выводит в консоль двумерный массив
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
+void SumStringMatrix(int[,] array) // выводит строку с наименьшей суммой элементов  в массиве
+{
+    int index = 0, minsum = 0;
+    for (int y = 0; y < array.GetLength(0); y++)
+    {
+        int sum = 0;
+        for (int x = 0; x < array.GetLength(1); x++)
+        {
+            sum += array[y, x];
+        }
+        // Console.WriteLine($"Сумма {y + 1} строки = {sum}");
+        if (y == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = y;
+        }
+    }
+    Console.WriteLine($"строка с наименьшей суммой элементов: {index + 1}. ");
+}
 
-
-
+int[,] array = FillArray2(4, 4, 1, 9);
+PrintArray2(array);
+SumStringMatrix(array);
