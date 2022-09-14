074e7e9641c6e399c26a77e39fc89341a0962b16
Андрей <pjanov84@mail.ru>
2022-09-14 17:13:36 +0300

    Added Task_54

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -9,4 +9,55 @@
 //  9 5 3 2
 //  8 4 4 2
 
+Console.Clear();
 
+int[,] FillArray2(int line, int col, int min, int max) //заполняет двумерный массив случайными числами от 1 до 9
+{
+    int[,] matr = new int[line, col];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintArray2(int[,] matr) //выводит в консоль двумерный массив
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array) // упорядочит по убыванию
+{
+    for (int y = 0; y < array.GetLength(0); y++)
+    {
+        for (int x = 0; x < array.GetLength(1) - 1; x++)
+        {
+            for (int m = 0; m < array.GetLength(1) - 1; m++)
+            {
+                if (array[y, m] < array[y, m + 1])
+                {
+                    int temp = 0;
+                    temp = array[y, m];
+                    array[y, m] = array[y, m + 1];
+                    array[y, m + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array = FillArray2(4, 4, 1, 9);
+PrintArray2(array);
+Console.WriteLine();
+SortArray(array);
+PrintArray2(array);
